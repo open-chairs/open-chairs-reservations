7c4ebb4a179d2b556fa2165a02e932dde05becd3
jasonasav <jasonasav@gmail.com>
2019-04-18 16:05:56 -0700

    wrote async await data seeding function

--- faker-seed.js
+++ faker-seed.js
@@ -13,22 +13,80 @@ var makeResTime = function () {
     var minute = ['00', '15', '30'];
     var resHour = hour[Math.floor(Math.random()*hour.length)];
     var resMin = minute[Math.floor(Math.random()*minute.length)];
-    
     return resHour + ':' + resMin + ':00'
-   
 }
 
 writer.pipe(fs.createWriteStream('testData.csv'))
 
 
+// async function myFunction() {
+//   while(/* my condition */) {
+//     const res = await db.getUser(email);
+//     logger.log(res);
+//   }
+// }
 
-for (var i = 1; i < 1000; i++) {
-  writer.write({
-    rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
-    date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
-    time: makeResTime()
-    })
+// myFunction().then(() => {
+//   /* do other stuff */
+// })
+
+// const createData = () => {
+//   var count = 1;
+//   let total = 10
+//   while (count < total ) {
+//   return new Promise(resolve => {
+//      count++
+//     resolve( writer.write({
+//       rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
+//       date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
+//       time: makeResTime()
+//       }) 
+//   )})
   
+
+// } 
+// }
+
+const createData = () => {
+  return new Promise(resolve => {
+    resolve( writer.write({
+      rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
+      date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
+      time: makeResTime()
+      }) 
+  )})
+  
+
+
 }
+
+
+async function asyncCall() {
+  // console.log('calling');
+  var result = await createData();
+  // console.log(result);
+  // expected output: 'resolved'
+}
+
+let count = 0;
+let total = 100;
+
+while ( count < total) {
+  count++;
+  asyncCall();
+}
+
+
+ 
+
+// for (var i = 1; i < 10000; i++) {
+//   writer.write({
+//     id: i,
+//     rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
+//     date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
+//     time: makeResTime()
+//     })
+  
+// }
 
 writer.end();
