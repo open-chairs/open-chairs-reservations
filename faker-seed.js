6356e4ec67914c024800d03778278b28f96fdb5a
jasonasav <jasonasav@gmail.com>
2019-04-18 17:07:46 -0700

    seeder is working, can seed 1 million no problem, will try for 40 million

--- faker-seed.js
+++ faker-seed.js
@@ -4,10 +4,6 @@ const fs = require('fs');
 var csvWriter = require('csv-write-stream')
 var writer = csvWriter()
 
-
-
-
-
 var makeResTime = function () {
     var hour = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
     var minute = ['00', '15', '30'];
@@ -18,75 +14,59 @@ var makeResTime = function () {
 
 writer.pipe(fs.createWriteStream('testData.csv'))
 
+const createReservation = () => {
+  return ({ rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
+  date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
+  time: makeResTime()})
+}
 
-// async function myFunction() {
-//   while(/* my condition */) {
-//     const res = await db.getUser(email);
-//     logger.log(res);
-//   }
-// }
+function writeMillionsOfReservations() {
+  let i = 1000000;
+  writeReservations();
+  function writeReservations() {
+    let ok = true;
+    do {
+      i--;
+      if (i === 0) {
+        writer.write(createReservation());
+      } else {
+        ok = writer.write(createReservation());
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', writeReservations);
+    }
+  }
+}
 
-// myFunction().then(() => {
-//   /* do other stuff */
-// })
+writeMillionsOfReservations();
 
+// Promisified create reservations
 // const createData = () => {
-//   var count = 1;
-//   let total = 10
-//   while (count < total ) {
 //   return new Promise(resolve => {
-//      count++
 //     resolve( writer.write({
-//       rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
-//       date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
-//       time: makeResTime()
+      // rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
+      // date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
+      // time: makeResTime()
 //       }) 
 //   )})
-  
-
-// } 
 // }
 
-const createData = () => {
-  return new Promise(resolve => {
-    resolve( writer.write({
-      rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
-      date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
-      time: makeResTime()
-      }) 
-  )})
-  
 
-
-}
-
-
-async function asyncCall() {
-  // console.log('calling');
-  var result = await createData();
-  // console.log(result);
-  // expected output: 'resolved'
-}
-
-let count = 0;
-let total = 100;
-
-while ( count < total) {
-  count++;
-  asyncCall();
-}
-
-
- 
-
-// for (var i = 1; i < 10000; i++) {
-//   writer.write({
-//     id: i,
-//     rest_id: (faker.random.number({'min': 1, 'max': 10000000})),
-//     date: faker.date.between('2019-01-01', '2019-03-31').toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }),
-//     time: makeResTime()
-//     })
-  
+// async function asyncCall() {
+//   console.log('calling');
+//   var result = await createData();
+//   console.log(result);
 // }
 
-writer.end();
+// let count = 0;
+// let total = 100;
+
+// while ( count < total) {
+//   count++;
+//   asyncCall();
+// }
+
+
+
+// writer.end();
