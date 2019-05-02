6f5c31503979cc6e4d85f4c39e70f2fe917b743f
jasonasav <jasonasav@gmail.com>
2019-05-01 17:31:36 -0700

    added compression

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const dbQuery = require('../database/index.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const compression = require('compression');
+
 app.use(cors());
 app.use(bodyParser.json())
+app.use(compression());
+
 const port = process.env.port || 3000;
 
 app.use('/:urlID', express.static(path.join(__dirname, '../public')));
